d6aa4011a35c261cc92ca9e533af3b3fe5f838fe
Digital Experience <64876963+DigitalExperience-code@users.noreply.github.com>
2023-11-22 01:53:46 -0300

    Bug Removed

--- app.py
+++ app.py
@@ -7,12 +7,3 @@ csrf = CSRFProtect(app)
 @app.route("/")
 def pagina_inicial():
     return "Solution Sprint - Fase 05"
-
-@app.route('/bug')                                                                                                                                
-def bad():                                                                                                                                        
-    try:                                                                                                                                          
-        raise TypeError()                                                                                                                         
-    except TypeError as e:                                                                                                                        
-        print(e)                                                                                                                                  
-    except TypeError as e:                                                                                                                        
-        print("Duplicado, ou seja, nunca vai entrar aqui.")
